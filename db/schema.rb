026c7af03529df6a1c47416b8f71769edad3fb37
kotoku-code <kennedy.kotoku@gmail.com>
2020-08-03 20:24:30 +0200

    added vote feature

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_03_122458) do
+ActiveRecord::Schema.define(version: 2020_08_03_132915) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 2020_08_03_122458) do
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "points", default: 1
+    t.float "hot_score", default: 0.0
     t.index ["user_id"], name: "index_links_on_user_id"
   end
 
@@ -39,7 +41,20 @@ ActiveRecord::Schema.define(version: 2020_08_03_122458) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "votes", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "link_id", null: false
+    t.integer "upvote"
+    t.integer "downvote"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["link_id"], name: "index_votes_on_link_id"
+    t.index ["user_id"], name: "index_votes_on_user_id"
+  end
+
   add_foreign_key "comments", "links"
   add_foreign_key "comments", "users"
   add_foreign_key "links", "users"
+  add_foreign_key "votes", "links"
+  add_foreign_key "votes", "users"
 end
