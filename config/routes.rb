1fdffa625816d1cc6af8f21ceb22f35935af31b2
kotoku-code <kennedy.kotoku@gmail.com>
2020-08-03 14:55:49 +0200

    added comment functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
   root 'links#index'
 
-  resources :links, except: :index
+  resources :links, except: :index do
+    resources :comments, only: [:create, :edit, :update, :destroy]
+    post :upvote, on: :member
+    post :downvote, on: :member
+  end
+
+  get '/comments' => 'comments#index'
+  get '/newest' => 'links#newest', as: :newest_links
 
   resources :sessions, only: [:new, :create] do
     delete :destroy, on: :collection
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_31_173130) do
+ActiveRecord::Schema.define(version: 2020_08_03_122458) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.integer "user_id", null: false
+    t.integer "link_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["link_id"], name: "index_comments_on_link_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "links", force: :cascade do |t|
     t.string "title"
@@ -29,5 +39,7 @@ ActiveRecord::Schema.define(version: 2020_07_31_173130) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "comments", "links"
+  add_foreign_key "comments", "users"
   add_foreign_key "links", "users"
 end
